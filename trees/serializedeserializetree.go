d7e71669153c475cff78216507e4e3a1eb45d5d0
gautamhitesh <gautamhitesh@hotmail.com>
2023-09-07 23:44:27 +0530

    Binary Tree serialization

--- trees/serializedeserializetree.go
+++ trees/serializedeserializetree.go
@@ -24,12 +24,15 @@ func insert(data int, ptr **Node) {
 	return
 }
 
+var serialized []int
+
 func serialize(ptr *Node) {
 	if ptr == nil {
 		fmt.Printf("%d ", -1)
 		return
 	}
 	fmt.Printf("%d ", ptr.data)
+	serialized = append(serialized, ptr.data)
 	serialize(ptr.left)
 	serialize(ptr.right)
 }
@@ -44,15 +47,49 @@ func inorder(ptr *Node) {
 	inorder(ptr.right)
 }
 
+func deserialize(ptr **Node, data int) {
+	if data == -1 {
+		return
+	}
+	if *ptr == nil {
+		//fmt.Println("Nil node")
+		*ptr = &Node{data, nil, nil}
+		return
+	}
+	if data < (*ptr).data {
+		//fmt.Println("going left")
+		deserialize(&(*ptr).left, data)
+	}
+	if data > (*ptr).data {
+		//fmt.Println("going right")
+		deserialize(&(*ptr).right, data)
+	}
+}
+
 func main() {
 	var root *Node
 	//r := utils.GenerateRandomArray(10)
 	//for i := 0; i < 10; i++ {
 	//	insert(r[i], &root)
 	//}
+	insert(20, &root)
+	insert(8, &root)
+	insert(4, &root)
 	insert(12, &root)
-	insert(13, &root)
+	insert(10, &root)
+	insert(14, &root)
 	inorder(root)
 	fmt.Println()
 	serialize(root)
+	fmt.Println()
+	fmt.Println(serialized)
+	var ds *Node
+	for i := 0; i < 6; i++ {
+		deserialize(&ds, serialized[i])
+	}
+	fmt.Println()
+	serialized = nil
+	serialize(root)
+	fmt.Println()
+	fmt.Println(serialized)
 }
